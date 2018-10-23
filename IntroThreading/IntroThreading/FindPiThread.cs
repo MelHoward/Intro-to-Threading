21fb5bc0e34144d46861677a49e807b98117f607
Melanie Howard <melshowa@ut.utm.edu>
2018-10-23 14:04:59 -0500

    Created FindPiThread

---
+++ IntroThreading/IntroThreading/FindPiThread.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+/*FindPiThread - this is the class that you will create to hold thread state.  It will also house your thread function
+ * An int holding the number of darts it needs to throw
+ * An int holding the count of darts that lands inside your dartboard (quarter)
+ * A Random - this will be used to generate your throws!
+ * 
+ * 
+ */
+namespace IntroThreading
+{
+    class FindPiThread
+    {
+        int numThrow;
+        int boardCount;
+        Random rnd;
+
+
+        public void ThrowDarts()
+        {
+
+        }
+    }
+}
